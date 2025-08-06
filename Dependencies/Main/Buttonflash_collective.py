c7161244eb3b3120f584cc109fb8520ca0217ddc
janfiess <janfiess@users.noreply.github.com>
2025-08-06 16:42:31 +0200

    Streamdeck integrated

--- Dependencies/Main/Buttonflash_collective.py
+++ Dependencies/Main/Buttonflash_collective.py
@@ -11,21 +11,21 @@ class Buttonflash:
 	# und von Play_individual_clip() in op.Clipgun_flash_individual getriggert
 	
 	def Play(self):
-		op.Clipgun_collective.par.Nextplayerid = (op.Clipgun_collective.par.Nextplayerid + 1) % 2
+		op.Clipgun_flash.par.Nextplayerid = (op.Clipgun_flash.par.Nextplayerid + 1) % 2
 		next_player_id = parent(3).par.Nextplayerid
 		clipname = parent().name
 		# print(f"clip to play: {clipname}")
 		
-		op.Clipgun_collective.pars(f"Latestkeypressed{next_player_id}")[0].val = clipname
+		op.Clipgun_flash.pars(f"Latestkeypressed{next_player_id}")[0].val = clipname
 		
 
 		# print("tetraeder not muted")
 	
-		moviefile_cwww_zone0 = op.Clipgun_collective.op(f"moviefilein_zone0_cwww_{int(next_player_id)}")
+		moviefile_cwww_zone0 = op.Clipgun_flash.op(f"moviefilein_zone0_cwww_{int(next_player_id)}")
 		# print(f"moviefile_cwww_zone0: {moviefile_cwww_zone0}")
 		self.startVideo(moviefile_cwww_zone0)
 			
-		moviefile_rgb_zone0 = op.Clipgun_collective.op(f"moviefilein_zone0_rgb_{int(next_player_id)}")
+		moviefile_rgb_zone0 = op.Clipgun_flash.op(f"moviefilein_zone0_rgb_{int(next_player_id)}")
 		# print(f"moviefile_rgb_zone0: {moviefile_rgb_zone0}")
 		self.startVideo(moviefile_rgb_zone0)
 
