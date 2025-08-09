15f0a8c871d3c359e49153d4365bf8e6478ecc6e
janfiess <janfiess@users.noreply.github.com>
2025-08-09 11:35:25 +0200

    cwww base läuft

--- Dependencies/Main/Button_base.py
+++ Dependencies/Main/Button_base.py
@@ -7,9 +7,16 @@ class Buttonbase:
 
 	def Play(self):
 		# print("button base")
-		moviefile = op.Clipgun_base.op("moviefilein_rgb")
 		op.Clipgun_base.par.Latestkeypressed = parent().name
 		
-		moviefile.par.cuepoint = 0
-		moviefile.par.cuepulse.pulse()
-		moviefile.par.play = 1
+		moviefile_rgb = op.Clipgun_base.op("moviefilein_rgb")
+		moviefile_rgb.par.cuepoint = 0
+		moviefile_rgb.par.cuepulse.pulse()
+		moviefile_rgb.par.play = 1
+
+		moviefile_cwww = op.Clipgun_base.op("moviefilein_cwww")
+		moviefile_cwww.par.cuepoint = 0
+		moviefile_cwww.par.cuepulse.pulse()
+		moviefile_cwww.par.play = 1
+
+
